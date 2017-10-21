72ab0bb84039fc19f3f6ad0882e00c88d674397f
Olivier Philippon <olivier@rougemine.com>
2017-10-21 14:48:21 +0100

    Add meta tags analysis step

--- scraper/doc_analyser.py
+++ scraper/doc_analyser.py
@@ -1,10 +1,16 @@
 
-from typing import Callable, NamedTuple
+from typing import Callable, List, NamedTuple
 from bs4 import BeautifulSoup
+
+
+class DocMetaTag(NamedTuple):
+    name: str
+    content: str
 
 
 class DocSummary(NamedTuple):
     page_title: str
+    meta_tags: List[DocMetaTag]
 
 
 class DocAnalyser:
@@ -14,11 +20,30 @@ class DocAnalyser:
 
     def analyse(self, url: str) -> DocSummary:
         html_doc: str = self.doc_fetcher(url)
-        self._soup = BeautifulSoup(html_doc, 'html.parser')
+        soup = BeautifulSoup(html_doc, 'html.parser')
 
         doc_summary_dict = {
-            'page_title': self._soup.title.string
+            'page_title': soup.title.string if soup.title else None,
+            'meta_tags': self._get_meta_tags(soup)
         }
 
         return DocSummary(**doc_summary_dict)
 
+    def _get_meta_tags(self, soup: BeautifulSoup) -> List[DocMetaTag]:
+        meta_tags = []
+        for meta in soup.find_all('meta'):
+            content: str = meta.get('content')
+
+            # Try various <meta> tags types
+            name: str = meta.get('name')
+            if name is None:
+                name = meta.get('http-equiv')
+            if name is None:
+                name = meta.get('property')
+            if name is None and 'charset' in meta.attrs:
+                name = 'charset'
+                content = meta.get('charset')
+
+            meta_tags.append(DocMetaTag(name=name, content=content))
+
+        return meta_tags
--- scraper/test_doc_analyser.py
+++ scraper/test_doc_analyser.py
@@ -11,6 +11,49 @@ def test_parsing_title():
     assert doc_summary.page_title == 'Hello Plum!'
 
 
+def test_parsing_meta():
+    html_doc = """
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+            <link rel="prefetch" href="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js">
+            
+            <meta http-equiv="imagetoolbar" content="false">
+        
+            <meta property="og:type" content="website">
+            <meta property="og:site_name" content="Python.org">
+            <meta property="og:title" content="PEP 274 -- Dict Comprehensions">
+            <meta property="og:description" content="The official home of the Python Programming Language">
+            
+            <meta name="keywords" content="Python programming language object oriented web free open source software license documentation download community">
+        </head>
+        <body>
+            PEP 274 -- Dict Comprehensions | Python.org
+        </body>
+    </html>
+    """
+
+    sut = DocAnalyser(doc_fetcher_mock(html_doc))
+    doc_summary = sut.analyse('https://www.python.org/dev/peps/pep-0274/')
+
+    expected_results = (
+        ('charset', 'utf-8'),
+        ('X-UA-Compatible', 'IE=edge'),
+        ('imagetoolbar', 'false'),
+        ('og:type', 'website'),
+        ('og:site_name', 'Python.org'),
+        ('og:title', 'PEP 274 -- Dict Comprehensions'),
+        ('og:description', 'The official home of the Python Programming Language'),
+        ('keywords', 'Python programming language object oriented web free open source software license documentation download community'),
+    )
+
+    assert len(doc_summary.meta_tags) == len(expected_results)
+    for tag_index, expected in enumerate(expected_results):
+        assert doc_summary.meta_tags[tag_index].name == expected[0] and doc_summary.meta_tags[tag_index].content == expected[1]
+
+
 def doc_fetcher_mock(expected_fetched_doc: str) ->Callable:
     def mock(url: str) ->str:
         return expected_fetched_doc
